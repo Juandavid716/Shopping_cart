cadc209da8af0f55141e70e8077e78c402de5d92
juandavid716 <jbojato@uninorte.edu.co>
2021-05-29 21:14:36 -0500

    first commit - main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header"
+//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+       <main>
+         <p> Product list </p>
+       </main>
+       <footer>
+         Create by Juan Bojato
+       </footer>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+return(
+    <header> 
+    <img alt="logo" src="https://img.icons8.com/dusk/64/000000/buy.png"/>
+      <p> Fast Shopping</p> 
+    </header>);
+}
+export default Header;
